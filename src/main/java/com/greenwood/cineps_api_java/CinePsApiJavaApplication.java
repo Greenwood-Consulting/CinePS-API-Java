da9fbe3b73b45f8166e174cc0463550e071cf220
Bertrand Boisvert <bb@gc2.fr>
2024-09-24 11:39:43 +0200

    initialisation de Spring

---
+++ src/main/java/com/greenwood/cineps_api_java/CinePsApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.greenwood.cineps_api_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinePsApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinePsApiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenwood/cineps_api_java/CinePsApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenwood.cineps_api_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinePsApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
